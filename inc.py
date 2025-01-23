efa8f0cf0987117e123f2ca1216b9aefed33bda6
sivakumar330 <sivasowdesh@gmail.com>
2025-01-23 10:12:55 +0530

    modified created files

---
+++ inc.py
@@ -0,0 +1,5 @@
+income = int(input("800income"))
+if(income>7000):
+    print("eligible for scholarship")
+else:
+    print("not eligible for scholarship")
---
+++ meg.py
@@ -0,0 +1,6 @@
+meghna=input()
+if(meghna=="died"):
+    print("surya meets priya")
+else:
+    print("meghna weds surya")
+    
