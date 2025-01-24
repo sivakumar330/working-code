b7f65f403773d3236fe3859703c89b48b88915ac
sivakumar330 <sivasowdesh@gmail.com>
2025-01-24 11:01:23 +0530

    modified loop.py file

--- loop.py
+++ loop.py
@@ -1,4 +1,10 @@
+a=[]
+print("enter number 101")
+for i in range(5):
+    num=int(input("Enter num"+str(i+1)))
+    a.append(num)
+print(a)
 sum = 0
-for i in range(1,6):
+for i in a:
     sum = sum+i
-print(sum)
+print(sum) 
